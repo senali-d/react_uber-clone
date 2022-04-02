92a83be047cbce7bc7e39af46388a9de4dc054c2
Senali <senalidilumika@gmail.com>
2022-04-03 01:33:40 +0530

    add map

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
 import tw from 'tailwind-styled-components'
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiemVuZGVzazk4NyIsImEiOiJjbDFpNzY0dGwwZWFxM2ZweXNjMGhxMDQ0In0.ieVpU3hBiBgEny-hzvr0QA';
+
 export default function Home() {
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 3
+    });
+  });
+
   return (
     <Wrapper>
-      <Map>Map</Map>
+      <Map id="map">Map</Map>
       <ActionItems>Start</ActionItems>
     </Wrapper>
   )
