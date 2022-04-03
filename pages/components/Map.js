5c9d13e7c4f7441b705164969e10d17e99976a1f
Senali <senalidilumika@gmail.com>
2022-04-04 00:54:26 +0530

    Add location to map

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -4,7 +4,7 @@ import tw from 'tailwind-styled-components'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiemVuZGVzazk4NyIsImEiOiJjbDFpNzY0dGwwZWFxM2ZweXNjMGhxMDQ0In0.ieVpU3hBiBgEny-hzvr0QA';
 
-const Map = () => {
+const Map = ({ pickupCoordiantes, dropoffCoordiantes }) => {
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -13,7 +13,31 @@ const Map = () => {
       center: [-99.29011, 39.39172],
       zoom: 3
     });
-  });
+
+    if (pickupCoordiantes) {
+      addToMap(map, pickupCoordiantes)
+    }
+
+    if (dropoffCoordiantes) {
+      addToMap(map, dropoffCoordiantes)
+    }
+
+    if (pickupCoordiantes && dropoffCoordiantes) {
+      map.fitBounds([
+        pickupCoordiantes, dropoffCoordiantes
+      ], {
+        padding: 60
+      }
+      );
+    }
+
+  }, [pickupCoordiantes, dropoffCoordiantes]);
+
+  const addToMap = (map, coordiantes) => {
+    const marker1 = new mapboxgl.Marker()
+      .setLngLat(coordiantes)
+      .addTo(map);
+  }
 
   return (
     <Wrapper id="map">
--- pages/confirm.js
+++ pages/confirm.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import tw from 'tailwind-styled-components'
 import Map from './components/Map'
 
 const Confirm = () => {
+
+  const [pickupCoordiantes, setPickupCoordiantes] = useState()
+  const [dropoffCoordiantes, setDropoffCoordiantess] = useState()
+
+  useEffect(() => {
+    getPickupCoordiantes()
+    getDropoffCoordiantes()
+  }, [])
+
+  const getPickupCoordiantes = () => {
+    const pickup = "Santa Monica"
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
+      new URLSearchParams({
+        access_token: "pk.eyJ1IjoiemVuZGVzazk4NyIsImEiOiJjbDFpNzY0dGwwZWFxM2ZweXNjMGhxMDQ0In0.ieVpU3hBiBgEny-hzvr0QA",
+        limit: 1
+      })
+    )
+      .then(response => response.json())
+      .then(data => {
+        setPickupCoordiantes(data.features[0].center)
+      })
+  }
+
+  const getDropoffCoordiantes = () => {
+    const dropoff = "Los Angeles"
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
+      new URLSearchParams({
+        access_token: "pk.eyJ1IjoiemVuZGVzazk4NyIsImEiOiJjbDFpNzY0dGwwZWFxM2ZweXNjMGhxMDQ0In0.ieVpU3hBiBgEny-hzvr0QA",
+        limit: 1
+      })
+    )
+      .then(response => response.json())
+      .then(data => {
+        setDropoffCoordiantess(data.features[0].center)
+      })
+  }
+
   return (
     <Wrapper>
-      <Map />
+      <Map pickupCoordiantes={pickupCoordiantes} dropoffCoordiantes={dropoffCoordiantes} />
       <RideContainer>
         Ride selector
         confirm Button
